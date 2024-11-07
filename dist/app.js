f59b8371fa4dbee5b90c61ce2b3085d65f7e02a7
EkaterinaGorbunova <ekaterina.nalitova@gmail.com>
2024-11-07 09:31:33 -0800

    added export a serverless function for Vercel (production)

--- dist/app.js
+++ dist/app.js
@@ -48,3 +48,7 @@ app.use("/admin", adminRoute_1.default);
 app.listen(port, () => {
     console.log(`🚀 Server has started on http://localhost:${port}`);
 });
+// Export a serverless function for Vercel (production)
+exports.default = (req, res) => {
+    app(req, res); // Pass the request and response to the Express app
+};
