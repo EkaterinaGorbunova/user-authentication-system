d454e0831b339011056709e5bc901bc8da9c8491
EkaterinaGorbunova <ekaterina.nalitova@gmail.com>
2024-11-07 09:56:41 -0800

    cleaned up

--- middleware/setBaseUrl.ts
+++ middleware/setBaseUrl.ts
@@ -3,6 +3,5 @@ import { Request, Response, NextFunction } from 'express';
 export const setBaseUrl = (req: Request, res: Response, next: NextFunction) => {
   // Get the base URL from the request headers
   req.baseUrl = `${req.protocol}://${req.get('host')}`;
-  console.log('req.baseUrl1:', req.baseUrl)
   next();
 };
