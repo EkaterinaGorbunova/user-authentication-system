a32fe5a2c6329cd6e145851ea3e7b3c49f0c64a6
EkaterinaGorbunova <ekaterina.nalitova@gmail.com>
2025-02-12 16:45:00 -0800

    Untrack dist folder

--- dist/middleware/setBaseUrl.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.setBaseUrl = void 0;
-const setBaseUrl = (req, res, next) => {
-    // Get the base URL from the request headers
-    req.baseUrl = `${req.protocol}://${req.get('host')}`;
-    next();
-};
-exports.setBaseUrl = setBaseUrl;
